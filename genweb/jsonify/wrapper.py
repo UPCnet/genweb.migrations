bbe5a678e10629aa7eb3ea77f5ca93beb21f17b4
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-01-08 16:51:04 +0100

    Enable export of translations (LinguaPlone)

--- genweb/jsonify/wrapper.py
+++ genweb/jsonify/wrapper.py
@@ -506,6 +506,14 @@ class Wrapper(dict):
         """
         if not hasattr(self._context, 'getCanonical'):
             return
+
+        translations = self.context.getTranslations()
+        self['_translations'] = {}
+
+        for lang in translations:
+            trans_obj = '/'.join(translations[lang][0].getPhysicalPath())[len(self.portal_path):]
+            self['_translations'][lang] = trans_obj
+
         self['_translationOf'] = '/'.join(self.context.getCanonical(
                                  ).getPhysicalPath())[len(self.portal_path):]
         self['_canonicalTranslation'] = self.context.isCanonical()
